aab0988272e3340c7f84d2cb27b10ef8092fe598
jedikarix <karolchecinski@wp.pl>
2019-05-24 18:48:22 +0200

    Add add_bidirectional_edge method to SimulationGraph

--- code/Simulation/SimulationGraph.py
+++ code/Simulation/SimulationGraph.py
@@ -1,21 +1,34 @@
+from typing import Tuple, Union
+
 import networkx as nx
 
 
-class SimulationGraph(nx.DiGraph()):
+class SimulationGraph(nx.DiGraph):
 
     def __init__(self, data=None, **attr):
         super().__init__(data, **attr)
 
-    def add_edge(self, u: int, v: int, trust: float = 0, data_source=None):
+    def add_edge(self, u: int, v: int, trust: float = 0) -> None:
         """
-        Add connection between agents
-        :param u: source agent
-        :param v: target agent
+        Add connection from agent u to agent v
+        :param u: source agent ID
+        :param v: target agent ID
         :param trust: level of trust (default 0)
-        :param data_source: (optional)
-        :return:
         """
-        attr_dict = dict()
-        attr_dict["trust"] = trust
-        attr_dict["data_source"] = data_source
-        super().add_edge(u_of_edge=u, v_of_edge=v, attr_dict=attr_dict)
+        super().add_edge(u_of_edge=u, v_of_edge=v, trust=trust)
+
+    def add_bidirectional_edge(self, u: int, v: int,
+                               trust: Union[Tuple[float, float], float] = (0, 0)) -> None:
+        """
+        Add bidirectional connection between u and v agents
+        :param u: agent ID
+        :param v: agent ID
+        :param trust: Pair (trust u in v, trust v in u) or single value if trust equal
+        """
+        u_trust, v_trust = trust if isinstance(trust, tuple) else (trust, trust)
+        self.add_edge(u, v, u_trust)
+        self.add_edge(v, u, v_trust)
+
+
+def read_simulation_graph(filename):
+    return SimulationGraph(nx.read_edgelist(filename, nodetype=int))
