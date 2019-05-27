cfec7cf5ce2bc83552b1bd02f19e90d10cc60dd6
jedikarix <karolchecinski@wp.pl>
2019-05-27 14:20:40 +0200

    Add docs to Knowledge

--- code/Simulation/Knowledge.py
+++ code/Simulation/Knowledge.py
@@ -18,17 +18,29 @@ class Knowledge:
         self.informations = list()
         self.agents_trust = dict()
 
-    def add_message(self, message: Message):
+    def add_message(self, message: Message) -> None:
+        """
+        Update knowledge based on received message
+        :param message: received message
+        """
         sender = message.sender
         sender_trust = self.agents_trust.get(sender, 0)
         info_trust = random() - 0.5  #
-        self.informations.append(KnowledgeInformation(message.body, sender_trust + info_trust, sender))
+        self.add_information(KnowledgeInformation(message.body, sender_trust + info_trust, sender))
         self.agents_trust[sender] = info_trust
 
-    def add_information(self, information: KnowledgeInformation):
+    def add_information(self, information: KnowledgeInformation) -> None:
+        """
+        Add information directly to knowledge
+        :param information:
+        """
         self.informations.append(information)
 
-    def get_random_information(self):
+    def get_random_information(self) -> KnowledgeInformation:
+        """
+        Get random information from knowledge, where probability of draw is proportional to trust.
+        :return: Drawn information
+        """
         reliable_informations = [information for information in self.informations if information.trust > 0]
         trust_exps = [exp(information.trust) for information in reliable_informations]
         sum_trust_exp = sum(trust_exps)
