1f4c1abc50b325ba46a5a96631b9abb0f9ebf1a2
lychanl <lycha_@poczta.onet.pl>
2019-05-28 23:36:42 +0200

    Removed temp file and updated SemanticAnalyser

--- code/SemanticAnalyser.py
+++ code/SemanticAnalyser.py
@@ -1,9 +1,10 @@
 import os
 import pickle
 import sys
-from multithreading import Lock
+from threading import Lock
 
 import numpy as np
+import torch
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'GenSen'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'SentEval'))
@@ -24,11 +25,39 @@ class SemanticAnalyser(object):
         )
 
         with open(os.path.join('GenSen', 'data', 'models', 'senteval.pickle'), 'rb') as file:
-            self.__evaluator = pickle.load(model_file)
+            self.__evaluator = pickle.load(file)
 
         self.__mutex = Lock()
 
-    def get_entailment(sentence1, sentence2):
+    def get_entailments_with_levels(self, sentence, sentences):
+        """Analyzes relation between a sentence and all in a collection
+
+        Args:
+            sentence: a sentence
+            sentences: a non-empty list of sentences
+
+        Returns:
+            entailment:
+                0 if entailed, 1 if neutral, 2 if contradicting for each element in sentences
+            level:
+                a non-negative value of how much this sentence is entailed with each element in sentences
+        """
+        self.__mutex.acquire()
+        _, encoded = self.__encoder.get_representation([sentence] + sentences, pool='last', return_numpy=True, tokenize=True)
+        input = np.concatenate((
+            np.repeat([encoded[0]], len(sentences), axis=0),
+            encoded[1:],
+            (np.repeat([encoded[0]], len(sentences), axis=0)) * encoded[1:]), axis=1)
+        output = self.__model_predict(input)
+        self.__mutex.release()
+
+        entailment = np.argmax(output, axis=1)
+        softmax = np.exp(output) / np.transpose(np.repeat([np.sum(np.exp(output), axis=1)], 3, axis=0))
+        level = np.max((np.argmax(output, axis=1) != 1) * softmax, axis=1)
+
+        return entailment, level
+
+    def get_entailment(self, sentence1, sentence2):
         """Analyzes relation between two sentences
 
         Args:
@@ -41,7 +70,19 @@ class SemanticAnalyser(object):
         self.__mutex.acquire()
         _, encoded = self.__encoder.get_representation([sentence1, sentence2], pool='last', return_numpy=True, tokenize=True)
         input = np.concatenate((encoded[0], encoded[1], encoded[0] * encoded[1]))
-        output = self.__evaluator.predict([input])
+        output = self.__model_predict([input])
         self.__mutex.release()
 
-        return output[0]
+        return np.argmax(output)
+
+    def __model_predict(self, input):
+        self.__evaluator.model.eval()
+        input = torch.FloatTensor(input).cuda()
+        yhat = []
+        with torch.no_grad():
+            for i in range(0, len(input), self.__evaluator.batch_size):
+                x = input[i:i + self.__evaluator.batch_size]
+                output = self.__evaluator.model(x)
+                yhat.append(output.data.cpu().numpy())
+        yhat = np.vstack(yhat)
+        return yhat
